862d3a9ed65e6e3dde44bf165a81e4207f43940e
KBungs <94625427+KBungs@users.noreply.github.com>
2022-01-27 23:25:51 +0700

    NekoCafe V.2
    NekoCafe V.2

--- Nekocafe.py
+++ Nekocafe.py
@@ -9,6 +9,15 @@ class Items:
 
     def remove(self):
         print(f"Remove {self.name} from list")
+        
+    def set_price(self, price):
+        self.price = price
+        print(f"{self.name} set price to {self.price}")
+        
+    def set_name(self, price):
+        self.price = price
+        print(f"Success set name to {self.name}")
+        
 
 
 class Cat:
@@ -19,6 +28,18 @@ class Cat:
 
     def feed(self):
         print(f"Successful feeding the {self.name}")
+        
+    def change_cat_name(self, name):
+        self.name = name
+        print(f"Success change the cat name to {self.name}")
+        
+    def change_cat_color(self, color):
+        self.color = color
+        print(f"Success change the cat color to {self.name}")
+        
+    def change_cat_type(self, type):
+        self.type = type
+        print(f"Success change the cat type to {self.name}")
 
 
 class Employee:
@@ -33,6 +54,22 @@ class Employee:
         
     def fire(self):
         print(f"Successful fire {self.name}")
+        
+    def set_id(self, id):
+        self.id = id
+        print(f"{self.name} set id to {self.id}")
+        
+    def set_name(self, name):
+        self.name = name
+        print(f"set new name to {self.name}")
+        
+    def set_salary(self, salary):
+        self.salary = salary
+        print(f"set salary to {self.salary}")
+        
+    def set_position(self, position):
+        self.position = position
+        print(f"set salary to {self.salary}")
 
 
 cats = [
@@ -74,6 +111,7 @@ menu = [
 ]
 
 employees = [
+    Employee(name="Sky Kung", position="Manager", salary="25000"),
     Employee(name="Emi Fukada", position="Waiter", salary="12000"),
     Employee(name="Sora Aoi", position="Chief", salary="21000"),
     Employee(name="Moei Amasuka", position="Cashier", salary="12000"),
